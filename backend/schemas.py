a15d9e54bff2cdb2c58d19cf72cf47dbaf0a4589
Daniil-Mah <daniilmahin17@gmail.com>
2026-05-14 13:38:02 +0300

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -2,14 +2,22 @@ from pydantic import BaseModel
 
 class BookBase(BaseModel):
     title: str
-    author: str
-    year: int
+    author_id: int
+    link_of_book: str
+    reading_status: bool = True
+    bookmark: str
+    tegs_id: int
 
 class BookCreate(BookBase):
     pass
 
-class BookUpdate(BookBase):
-    pass
+class BookUpdate(BaseModel):
+    title: str
+    author_id: int
+    link_of_book: str
+    reading_status: bool
+    bookmark: str
+    tegs_id: int
 
 class BookOut(BookBase):
     id: int

d76528de8be6799a84a9763cbbb03c3ab0fa920e
Daniil-Mah <daniilmahin17@gmail.com>
2026-05-14 13:09:37 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,25 @@
-from peewee import Model, CharField, IntegerField
+from peewee import *
 from .db import db
 
-class Book(Model):
-    title = CharField()
-    author = CharField()
-    year = IntegerField()
-
+class BaseTable(Model):
     class Meta:
         database = db
+
+class Users(BaseTable):
+    nickname = CharField()
+    role_adm = BooleanField(default=False)
+    password = CharField()
+
+class Author(BaseTable):
+    nickname = CharField()
+
+class Tegs(BaseTable):
+    teg = CharField()
+
+class Books(BaseTable):
+    title = CharField()
+    author_id = IntegerField()
+    link_of_book = CharField()
+    reading_status = BooleanField(default=True)
+    bookmark = CharField()
+    tegs_id = IntegerField()

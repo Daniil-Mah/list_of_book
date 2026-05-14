27abdde5b583d02efa0132e76b0e5e3f2f6b7af6
Daniil-Mah <daniilmahin17@gmail.com>
2026-05-14 13:50:17 +0300

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,8 @@
 from peewee import MySQLDatabase
 
 # Настройте данные для подключения под себя
-db = MySQLDatabase(
-    "books_db",       # имя вашей базы
+db = MySQLDatabase("list_of_book_db",
     user="youruser",
     password="yourpassword",
     host="localhost",
-    port=3306
-)
+    port=3306)

900c36ccb0ea42b33953e4ec17a89e7b1de6d88b
Daniil-Mah <daniilmahin17@gmail.com>
2026-05-15 12:11:11 +0300

    Delete backend directory

--- backend/db.py
+++
@@ -1,8 +0,0 @@
-from peewee import MySQLDatabase
-
-# Настройте данные для подключения под себя
-db = MySQLDatabase("list_of_book_db",
-    user="youruser",
-    password="yourpassword",
-    host="localhost",
-    port=3306)
